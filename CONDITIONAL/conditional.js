9494b1258d9cb70f47ea93dfd8cd015680f8e10b
quangdangcodegym <quang.dang@codegym.vn>
2022-12-29 20:11:28 +0700

    conditional...

---
+++ CONDITIONAL/conditional.js
@@ -0,0 +1,17 @@
+let number = 0;
+
+// number == '0' ==> trả về TRUE, nên khối if se thực hiện
+if(number =='0'){
+    console.log("A....... bằng 0")
+}
+
+// number === '0' ==> trả về FALSE, nên khối if se thực hiện
+if(number === '0'){
+    console.log("B....... bằng 0")
+}
+
+if(number==='0'){
+    console.log("C IF....... bằng 0")
+}else{
+    console.log("C ELSE....... bằng 0")
+}
---
+++ VARIABLE/variable.js
@@ -0,0 +1,57 @@
+var a = 5;      // số nguyên integer
+let b = 5.0     // số thập phân, decimal
+let c = "JS2022"    // kiểu chuỗi - string
+let d = 'c'         // kiểu kí tự - character
+var e = true;       // kiểu đúng sai - boolean
+var e1 = false;
+
+a = "Hello";
+console.log(a);
+console.log(typeof a)       // toán tử để lấy kiểu dữ liệu của biến
+
+let firstName1 = "Quang Dang";
+
+const max = 10;     // Khai báo hằng số thì dùng const
+//  max = 12;       // hằng số thì không thể gán giá trị
+console.log(max);
+
+let x1 = "";        // "",0, null, underfine ==> quy về đung/sai là FALSE
+console.log(Boolean(x1));
+
+let x2 = 30;        // nếu có giá trị => quy về dung/sai thì là TRUE
+console.log(Boolean(x2));
+
+// ------------------OPERATOR
+// Toán học: +,-,*,/
+
+let count = 0;
+// count++;         // count = count + 1;
+count = count+1;
+count *= 5;         // count = count * 5;
+console.log(count)
+
+
+// Cộng chuỗi
+let firstName = 'Đáng" Va';
+let lastName = "Quang";
+
+let fullName = firstName + " " +lastName;
+
+console.log(fullName)
+
+let number1 = 5;
+let number2 = 5;
+console.log(number1+number2)
+
+
+let number3;        // underfine
+console.log(number3)
+number1 = number1 + number3;
+console.log( number1)
+
+let number4 = null      // có giá trị null
+number4 = number1 + number3;
+console.log(number4)
+
+
+
